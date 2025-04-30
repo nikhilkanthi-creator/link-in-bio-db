0c7b31b4c7d0bf4bd87b5379b2a026c9c13ab78b
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-30 01:10:29 +0000

    100%! missing formatting though

--- app/controllers/linkinbio_controller.rb
+++ app/controllers/linkinbio_controller.rb
@@ -1,6 +1,7 @@
 class LinkinbioController < ApplicationController
 
   def homepage
+    @list_of_links = Item.all.order({ :created_at => :desc})
     render({ :template => "page_templates/home"})
   end
 end
